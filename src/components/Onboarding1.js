c33e62249d2a91421c42cd0d16350d7b1ed03f68
wobwobbird <36921161+wobwobbird@users.noreply.github.com>
2025-11-28 17:49:15 +0000

    Finished mvp for onboarding 1

--- src/components/Onboarding1.js
+++ src/components/Onboarding1.js
@@ -1,23 +1,104 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image, Pressable } from "react-native";
+import phone from '../../assets/phone-cal.png';
+import fold from '../../assets/fold-logo.png';
 
 export default function Onboarding1() {
     return (
-        <View>
-            <View style={style.logo} >
-                <Text>Fold</Text>
-            </View>
+        <View style={styles.screenHolder}>
+            <Image
+                source={fold}
+                style={styles.logo}
+            />
+            <Image
+                source={phone}
+                style={styles.phoneImage}
+            />
+            <Text style={styles.text2}>Find</Text>
+            <Text style={styles.text3}>Control & hope</Text>
+            <Text style={styles.text4}>Beyond gambling</Text>
+            <Pressable
+                style={styles.button}
+            >
+                <Text style={styles.buttonText}>Lets Get Started</Text>
 
+            </Pressable>
+            <Text style={styles.text5}>Already have an account?</Text>
         </View>
     )
 
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     screenHolder: {
-        // flex: 1,
         justifyContent: "flex-start",
+        alignItems: "center",
+        flex: 1,
     },
     logo: {
-        backgroundColor: "green",
+        height: 100,
+        width: 100,
+
+        shadowColor: "rgba(0, 0, 0, 0.61)",
+        shadowOffset: { width: 3, height: 3 },
+        shadowOpacity: 0.25,
+        shadowRadius: 5,
+        elevation: 5,
+    },
+    phoneImage: {
+        width: 200,
+        height: 400,
+
+        shadowColor: "rgba(0, 0, 0, 0.61)",
+        shadowOffset: { width: 3, height: 3 },
+        shadowOpacity: 0.25,
+        shadowRadius: 5,
+        elevation: 5,
+
+    },
+    text2: {
+        marginTop: 30,
+        fontSize: 20,
+        fontFamily: 'Rubik',
+        fontWeight: 300,
+        color: "white",
+    },
+    text3: {
+        fontSize: 40,
+        fontFamily: 'Rubik',
+        color: "white",
+    },
+    text4: {
+        fontSize: 25,
+        fontFamily: 'Rubik',
+        color: "white",
+    },
+    button: {
+        marginTop: 40,
+        backgroundColor: "rgba(22, 22, 22, 0.85)",
+        height: 50,
+        width: "80%",
+        borderRadius: 25,
+
+        justifyContent: "center",
+        alignItems: "center",
+        
+        shadowColor: "rgb(0, 0, 0)",
+        shadowOffset: { width: 3, height: 3 },
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        elevation: 5,
+    },
+    buttonText: {
+        fontSize: 20,
+        fontFamily: 'Rubik',
+        color: "white",
+    },
+    text5: {
+        marginTop: 10,
+        fontSize: 18,
+        fontWeight: 400,
+        fontFamily: 'Rubik',
+        color: "white",        
     }
+
 })
