a5c96f14901992cc657a4ec56fcfc7fb90ca91ea
wobwobbird <36921161+wobwobbird@users.noreply.github.com>
2025-11-29 15:10:45 +0000

    Updated app name

--- App.js
+++ App.js
@@ -46,7 +46,6 @@ export default function App() {
       <AppContent/>
     </AppProvider>
   )
-
 }
 
 const styles = StyleSheet.create({
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import { registerRootComponent } from 'expo';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
